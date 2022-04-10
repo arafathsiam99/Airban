154601ccddb3f6611b7770a9831da3ac0debf423
arafathsiam99 <md.arafathislamsiamnstuam14@gmail.com>
2022-04-11 01:07:38 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Blog from "./Pages/Blog/Blog";
+import Feature from "./Pages/Feature/Feature";
+import Footer from "./Pages/Footer/Footer";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <Navbar></Navbar>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/blog">
+            <Blog></Blog>
+          </Route>
+          <Route exact path="/feature">
+            <Feature></Feature>
+          </Route>
+        </Switch>
+          <Footer></Footer>
+      </Router>
     </div>
   );
 }
